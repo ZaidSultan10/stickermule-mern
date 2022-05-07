9e5cab6ee0027147b818fb81f35a4d90cbde7642
ZaidSultan10 <zaidsultan09@gmail.com>
2022-05-08 03:03:16 +0400

    added all options for navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,21 +2,27 @@ import React from 'react'
 import './_navbar.scss'
 import Logo from '../../assets/logo.png'
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 
 const Navbar = () => {
+
+  const [toggleProductArrow,setToggleProductArrow] = React.useState(false)
+  const [toggleToolsArrow,setToggleToolsArrow] = React.useState(false)
+
   return (
     <div className='navbar'>
         <div className='navbar__container'>
             <div className='navbar__left'>
                 <img src={Logo} className='navbar__left__logo' />
-                <a href='#'>Samples</a>
-                <a href='#'>Deals</a>
+                <p onClick={() => setToggleProductArrow(!toggleProductArrow)}>Products <span>{toggleProductArrow ? <ArrowDropUpIcon /> : <ArrowDropDownIcon /> }</span></p>
+                <p onClick={() => setToggleToolsArrow(!toggleToolsArrow)}>Tools <span>{toggleToolsArrow ? <ArrowDropUpIcon /> : <ArrowDropDownIcon /> }</span></p>
                 <a href='#'>Samples</a>
                 <a href='#'>Deals</a>
             </div>
             <div className='navbar__right'>
                 <ShoppingCartIcon className='navbar__right__cart' />
-                <a href='#'>Login</a>
+                <a href='#'>Log in</a>
                 <a href='#'>Sign up</a>
             </div>
         </div>
